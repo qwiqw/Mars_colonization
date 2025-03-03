b2647bacd67e13fe94557c2fcc907a6642457f88
qwiqw <90405747+qwiqw@users.noreply.github.com>
2025-03-03 18:07:47 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
 from flask import Flask, render_template, request
+from flask_wtf import FlaskForm
+from werkzeug.utils import redirect
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'qwe'
+
+
+class LoginForm(FlaskForm):
+    astronaut_id = StringField('ID астронавта', validators=[DataRequired()])
+    astronaut_password = PasswordField('Пароль астронавта', validators=[DataRequired()])
+    capitan_id = StringField('ID капитана', validators=[DataRequired()])
+    capitan_password = PasswordField('Пароль капитана', validators=[DataRequired()])
+    access = SubmitField('Доступ')
 
 
 @app.route('/')
@@ -68,6 +81,16 @@ def list_prof(lst):
         'list': lst
     }
     return render_template('list_prof.html', **context)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if request.method == 'GET':
+        return render_template('login.html', form=form)
+    elif request.method == 'POST':
+        if form.validate_on_submit():
+            return redirect('/')
 
 
 if __name__ == '__main__':
