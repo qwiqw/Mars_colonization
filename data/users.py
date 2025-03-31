3222a2b00b10a9bfcdb6c910cd9d063ba0156f76
qwiqw <90405747+qwiqw@users.noreply.github.com>
2025-03-31 15:30:40 +0300

    Update users.py

--- data/users.py
+++ data/users.py
@@ -5,7 +5,7 @@ from sqlalchemy_serializer import SerializerMixin
 from .db_session import SqlAlchemyBase
 
 
-class User(SqlAlchemyBase):
+class User(SqlAlchemyBase, SerializerMixin):
     __tablename__ = 'users'
     id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
     surname = sqlalchemy.Column(sqlalchemy.String, nullable=False)
