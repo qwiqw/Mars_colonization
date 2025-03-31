c6ea24e51818bd2c30e5baab3a32aa0d9aeafc59
qwiqw <90405747+qwiqw@users.noreply.github.com>
2025-03-31 15:30:21 +0300

    Update jobs.py

--- data/jobs.py
+++ data/jobs.py
@@ -3,7 +3,7 @@ from .db_session import SqlAlchemyBase
 import sqlalchemy.orm as orm
 from sqlalchemy_serializer import SerializerMixin
 
-class Jobs(SqlAlchemyBase):
+class Jobs(SqlAlchemyBase, SerializerMixin):
     __tablename__ = 'jobs'
     id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
     team_leader = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey('users.id'))
