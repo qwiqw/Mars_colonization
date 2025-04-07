28c011ff8caf09a6d422ba7c53136efd8e73ba1f
qwiqw <90405747+qwiqw@users.noreply.github.com>
2025-04-07 17:21:37 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
 import requests
 
 # праивльный гет запрос
-print(requests.get('http://127.0.0.1/api/v2/users/1').json())
+print(requests.get('http://127.0.0.1:8080/api/v2/users/1').json())
 # праильный делит запрос
-print(requests.delete('http://127.0.0.1/api/v2/users/1').json())
+print(requests.delete('http://127.0.0.1:8080/api/v2/users/1').json())
 # правильный пост запрос
 user = {
     'surname': 'surname',
@@ -15,11 +15,11 @@ user = {
     'email': 'email',
     'hashed_password': 'hashed_password',
 }
-print(requests.post('http://127.0.0.1/api/v2/users', json=user).json())
+print(requests.post('http://127.0.0.1:8080/api/v2/users', json=user).json())
 # неправильный гет запрос
-print(requests.get('http://127.0.0.1/api/v2/users/-1').json())
+print(requests.get('http://127.0.0.1:8080/api/v2/users/-1').json())
 # непраильный делит запрос
-print(requests.delete('http://127.0.0.1/api/v2/users/-1').json())
+print(requests.delete('http://127.0.0.1:8080/api/v2/users/-1').json())
 # неправильный пост запрос
 user1 = {}
-print(requests.post('http://127.0.0.1/api/v2/users', json=user).json())
+print(requests.post('http://127.0.0.1:8080/api/v2/users', json=user).json())
